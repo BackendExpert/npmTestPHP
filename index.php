6bd057320f172ca620dafac2ddcca557f73f993c
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-01-25 20:51:40 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- index.php
+++ index.php
@@ -2,4 +2,24 @@
 
 <div class="site-container">
     <button class="jkbtn jkbtn-blue">Hi all</button>
+
+    <button class="jkbtn jkbtn-blue" id="jkbtn-popup">Click Here</button>
+
+    <div class="jk-modal" id= "modalJK">
+        <div class="jk-modal-content">
+            <div class="jk-mheader">
+                <h1>JKCSS Modal</h1>
+            <div>
+            <hr>
+            <div class="jk-mbody">
+                <p>A CSS Framework<p>
+                <p>JKCSS is a one of Framework<p>
+            <div>
+            <hr>
+            <div class="jk-mfooter">
+                <button class="jkbtn jkbtn-green" id="closeModel">Close</button>
+            <div>
+        <div>
+    </div>
+
 </div>
